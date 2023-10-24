e2a7cf3caff64c7d6dd88f23fbc792ce6e47999b
vvborsce <borv38@gmail.com>
2023-10-24 17:01:12 +0300

    Added code to Main Page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,4 @@
 from .pages.main_page import MainPage
-import selenium.webdriver.common.by
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
