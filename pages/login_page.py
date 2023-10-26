a6ddbc03806989ffa15fc7e3950e45dc5be9854c
vvborsce <borv38@gmail.com>
2023-10-26 12:56:52 +0300

    Added URL comparsion_last version

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,7 +12,9 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         login_u = self.browser.current_url
         assert LoginPageLocators.LOGIN_URL in login_u, "Login is not presented in the current URL"
+
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_REGISTER), "Register form is not presented"
