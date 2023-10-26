80b4f859b891d3bec345bcd2c09fcfcfd4207ffd
vvborsce <borv38@gmail.com>
2023-10-26 12:43:00 +0300

    Added URL comparsion_last version

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,21 +2,22 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
-      #  self.should_be_login_form()
-       # self.should_be_register_form()
+
+    #  self.should_be_login_form()
+    # self.should_be_register_form()
 
     def should_be_login_url(self):
         login_u = self.browser.current_url
-        print(login_u)
-        assert LoginPageLocators.LOGIN_URL in login_u, "Login is not presented in the current URL"
+        assert LoginPageLocators.LOGIN_URL in login_u.text, "Login is not presented in the current URL"
 #
 #   # def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
- #       assert True
+# реализуйте проверку, что есть форма логина
+#       assert True
 #
-  #  def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-   #     assert True
+#  def should_be_register_form(self):
+# реализуйте проверку, что есть форма регистрации на странице
+#     assert True
