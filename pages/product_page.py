acbfe9fdd8f21b04915aa1ceb3c633444315dec8
vvborsce <borv38@gmail.com>
2023-11-01 17:21:14 +0300

    Added move to the page without separate functions

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,11 +10,11 @@ class ProductPage(BasePage):
         a_message = self.browser.find_element(*ProductPageLocators.ADD_MESSAGE)
         print(a_message.text)
         full_book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
-        assert full_book_name in a_message.text, "Ошибка"
+        assert full_book_name.text in a_message.text, "Ошибка"
         print("Товар добавлен успешно")
 
     def price_is_ok(self):
         c_price = self.browser.find_element(*ProductPageLocators.CART_PRICE)
         subj_price = self.browser.find_element(*ProductPageLocators.PRICE)
-        assert subj_price == c_price.text, "Price not looks good"
+        assert subj_price.text == c_price.text, "Price not looks good"
         print("Стоимость товара в корзине соответствует стоимости товара")
