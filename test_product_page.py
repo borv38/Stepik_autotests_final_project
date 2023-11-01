c35bfc3250a4ccd59d2407bbc6c01ced3a2249f2
vvborsce <borv38@gmail.com>
2023-11-01 13:06:09 +0300

    Added move to the page without separate functions

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,5 @@
+import time
+
 from .pages.product_page import ProductPage
 
 def test_go_to_product_page(browser):
@@ -6,6 +8,7 @@ def test_go_to_product_page(browser):
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
+    time.sleep(10)
     page.product_added()
     page.price_is_ok()
 
