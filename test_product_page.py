5971ae131bc7af92b13a648d90ec5a03c9a67225
vvborsce <borv38@gmail.com>
2023-11-01 16:18:54 +0300

    Added move to the page without separate functions

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from .pages.locators_product import ProductPageLocators
 from .pages.product_page import ProductPage
 
 def test_go_to_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
