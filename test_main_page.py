d4121cf8746e0a55c2faee0a25d36099181f1440
vvborsce <borv38@gmail.com>
2023-11-02 17:12:19 +0300

    Added move to the page without separate functions

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
+import pytest
+
+from pages.base_page import BasePage
 from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -11,8 +15,8 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.checkout_button_exists()
     basket_page.empty_cart()
 
-# from .pages.login_page import LoginPage
-#from selenium.webdriver.common.by import By
+
+# from selenium.webdriver.common.by import By
 
 # def test_guest_can_go_to_login_page(browser):
 #    link = "http://selenium1py.pythonanywhere.com/"
@@ -20,21 +24,22 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 #    page.open()
 #    page.go_to_login_page()
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
-#   #  login_page = page.go_to_login_page()
-#    # login_page.should_be_login_page()
-#
-# def test_guest_should_see_login_link(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = BasePage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+#  login_page = page.go_to_login_page()
+# login_page.should_be_login_page()
+    def test_guest_should_see_login_link(browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.should_be_login_link()
 #
 # def test_login_url(browser):
 #     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -52,4 +57,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 #     page = LoginPage(browser, link)
 #     page.open()
 #     page.should_be_register_form()
-
