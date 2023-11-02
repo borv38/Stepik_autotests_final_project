5d62cabebfc4f8cd84d17c024e0146154f5ef0e8
vvborsce <borv38@gmail.com>
2023-11-02 21:50:37 +0300

    Final task completed

--- test_product_page.py
+++ test_product_page.py
@@ -65,7 +65,7 @@ def test_guest_can_add_product_to_basket(self, browser):
 #     page.open()
 #     page.should_be_login_link()
 #
-@pytest.mark.need_review#
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
