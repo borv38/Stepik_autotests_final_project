45bd79ce5389cf8e897f9909ce743dc6e4bb18bf
vvborsce <borv38@gmail.com>
2023-11-02 16:04:04 +0300

    Added move to the page without separate functions

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,15 @@ from .pages.base_page import BasePage
 from .pages.main_page import MainPage
 
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasePage(browser, link)
+    page.open()
+    cart_page = page.go_to_the_cart()
+    cart_page.checkout_button_exists()
+    cart_page.empty_cart()
+
+
 # from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
@@ -45,12 +54,5 @@ from selenium.webdriver.common.by import By
 #     page.open()
 #     page.should_be_register_form()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = BasePage(browser, link)
-    page.open()
-    cart_page = page.go_to_the_cart()
-    cart_page.checkout_button_exists()
-    cart_page.empty_cart()
 
 # def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
