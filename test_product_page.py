ae0d0bbb721789f134db1d8668a9e4b0529d2c26
vvborsce <borv38@gmail.com>
2023-11-02 23:01:50 +0300

    Final task completed

--- test_product_page.py
+++ test_product_page.py
@@ -69,7 +69,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.need_review  # данный тест упадет и будет отмечен как FAILED потомучто в обучении указан не существующий селектор
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
