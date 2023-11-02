334ce3639450b4a351051c2d96c6aad9b9727d0c
vvborsce <borv38@gmail.com>
2023-11-02 16:02:29 +0300

    Added move to the page without separate functions

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from .pages.main_page import MainPage
 
 
 # from .pages.login_page import LoginPage
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 
 # def test_guest_can_go_to_login_page(browser):
 #    link = "http://selenium1py.pythonanywhere.com/"
