7a5734e57ac725f19c97afd721be67c000c624f5
vvborsce <borv38@gmail.com>
2023-11-02 11:34:12 +0300

    Added move to the page without separate functions

--- test_product_page.py
+++ test_product_page.py
@@ -6,19 +6,10 @@ from .pages.locators_product import ProductPageLocators
 from .pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.parametrize('link', ["0", "1","2", "3", "4", "5", "6", "7", "8", "9"])
 
 def test_go_to_product_page(browser, link):
-    link = {link}
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, link)
     page.open()
     time.sleep(2)
@@ -27,3 +18,4 @@ def test_go_to_product_page(browser, link):
     time.sleep(5)
     page.product_added()
     page.price_is_ok()
+
