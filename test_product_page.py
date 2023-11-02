725d8bc98081abab326238ff584c73a817c62d56
vvborsce <borv38@gmail.com>
2023-11-02 16:45:05 +0300

    Added move to the page without separate functions

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import time
 
 import pytest
 
+from pages.basket_page import BasketPage
 from .pages.locators_product import ProductPageLocators
 from .pages.product_page import ProductPage
 
@@ -52,8 +53,19 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_can_go_to_login_page_from_product_page (browser):
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_the_cart()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.checkout_button_exists()
+    basket_page.empty_cart()
