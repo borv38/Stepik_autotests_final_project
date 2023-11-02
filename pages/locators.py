a99db6e0c1a244f169a32bb7fb37d7cd9b45a2d2
vvborsce <borv38@gmail.com>
2023-11-02 14:02:01 +0300

    Added move to the page without separate functions

--- pages/locators.py
+++ pages/locators.py
@@ -1,14 +1,15 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+# class MainPageLocators():
+#  LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators():
     LOGIN_URL = "login"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_REGISTER = (By.CSS_SELECTOR, "#register_form")
+
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
