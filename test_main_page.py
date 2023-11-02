78ef9643463b18ebb636ef61dce70e6705b5f063
vvborsce <borv38@gmail.com>
2023-11-02 15:48:20 +0300

    Added move to the page without separate functions

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from pages.main_page import MainPage
+from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 #from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
